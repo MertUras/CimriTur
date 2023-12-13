de781966a86e810b73b74921fd691ea204966719
MertUras <mert_uras@windowslive.com>
2023-12-13 21:51:22 +0300

    Content changed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import videoBg from '../assets/videoBg.mp4'
+//import videoBg from '../assets/videoBg.mp4'
 import Countdown from './Countdown';
 import Logo from '../assets/black-removebg-preview.png'
+import FotoBg from '../assets/fotoBg2.jpg';
 const Main=()=>{
     return(
     <div className='main'>
-        <video src={videoBg} autoPlay loop muted> </video>
+         <img src={FotoBg} alt="Background" className="background-image" />
         <div className="content">
             <img id='logo' src={Logo} />
             <h1>Hayatınıza Yeni Bir Rota Oluşturun</h1>
