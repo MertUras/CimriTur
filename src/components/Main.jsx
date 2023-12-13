7563ebc5c3bff54af758b6a29560219ed563e701
MertUras <mert_uras@windowslive.com>
2023-12-13 23:01:41 +0300

    Contents Changed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 import Countdown from './Countdown';
 import Logo from '../assets/black-removebg-preview.png'
 import FotoBg from '../assets/fotoBg2.jpg';
+import InstagramIcon from '../assets/instagram.png';
 const Main=()=>{
     return(
     <div className='main'>
          <img src={FotoBg} alt="Background" className="background-image" />
         <div className="content">
             <img id='logo' src={Logo} />
-            <h1>Hayatınıza Yeni Bir Rota Oluşturun</h1>
+            <h1>Sadece Rota Değil, Yepyeni Bir Başlangıç</h1>
             <Countdown />
+
         </div>
     </div>
     )
