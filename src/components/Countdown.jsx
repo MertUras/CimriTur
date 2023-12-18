9030a009b634468d5787735f23800a60f1934869
MertUras <mert_uras@windowslive.com>
2023-12-18 15:52:16 +0300

    Countdown Algorithm Changed and Unused Values rmvd

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -64,7 +64,7 @@ const Countdown = () => {
 };
 
 export default Countdown;
-*/
+
 
 import React, { useEffect, useState } from 'react';
 import '../index.css'; // Import your styles from index.css
@@ -74,6 +74,66 @@ const Countdown = () => {
   const deadlineTurkeyTime = new Date();
   deadlineTurkeyTime.setMonth(deadlineTurkeyTime.getMonth() + 4);
   deadlineTurkeyTime.setHours(23, 59, 0, 0);
+
+  const [countdownValues, setCountdownValues] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = deadlineTurkeyTime - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdownValues({ days, hours, minutes, seconds });
+
+      if (distance < 0) {
+        clearInterval(interval);
+        setCountdownValues({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [deadlineTurkeyTime]);
+
+  return (
+    <div className="countdown">
+      <div className="countdown-item">
+        <div className="countdown-label">Gün</div>
+        <div className="countdown-value">{countdownValues.days}</div>
+      </div>
+      <div className="countdown-item">
+        <div className="countdown-label">Saat</div>
+        <div className="countdown-value">{countdownValues.hours}</div>
+      </div>
+      <div className="countdown-item">
+        <div className="countdown-label">Dakika</div>
+        <div className="countdown-value">{countdownValues.minutes}</div>
+      </div>
+      <div className="countdown-item">
+        <div className="countdown-label">Saniye</div>
+        <div className="countdown-value">{countdownValues.seconds}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
+
+*/
+import React, { useEffect, useState, useMemo } from 'react';
+import '../index.css'; // Import your styles from index.css
+
+const Countdown = () => {
+  // Memoize the deadlineTurkeyTime calculation
+  const deadlineTurkeyTime = useMemo(() => {
+    const deadline = new Date();
+    deadline.setMonth(deadline.getMonth() + 4);
+    deadline.setHours(23, 59, 0, 0);
+    return deadline;
+  }, []);
 
   const [countdownValues, setCountdownValues] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 //import videoBg from '../assets/videoBg.mp4'
 import Countdown from './Countdown';
-import Logo from '../assets/black-removebg-preview.png'
+
 import FotoBg from '../assets/fotoBg2.jpg';
-import InstagramIcon from '../assets/instagram.png';
 const Main=()=>{
     return(
     <div className='main'>
          <img src={FotoBg} alt="Background" className="background-image" />
         <div className="content">
-            <img id='logo' src={Logo} />
+            
             <h1>Sadece Rota Değil, Yepyeni Bir Başlangıç</h1>
             <Countdown />
 
